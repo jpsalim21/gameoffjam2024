258844c8c85f0a2b89d81aa28931ab238471c8c7
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-11-05 19:08:23 -0300

    Coloca tempo no slow motion

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -5,9 +5,12 @@ var estaSlow := false
 signal slowMotionSignal(valor : bool)
 
 func slowMotion():
-	estaSlow = !estaSlow
-	slowMotionSignal.emit(estaSlow)
 	if estaSlow:
-		InimigoBase.multiplicadorTemporal = 0.2
-	else:
-		InimigoBase.multiplicadorTemporal = 1
+		return
+	estaSlow = true
+	slowMotionSignal.emit(estaSlow)
+	InimigoBase.multiplicadorTemporal = 0.2
+	await get_tree().create_timer(3).timeout
+	InimigoBase.multiplicadorTemporal = 1
+	estaSlow = false
+	slowMotionSignal.emit(estaSlow)
