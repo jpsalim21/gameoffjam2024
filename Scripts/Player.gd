2d5e47e1c7b3278a33306443c4e7ceef1adb1c1a
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-11-13 19:53:36 -0300

    Arruma ataque do personagem

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		direction = Input.get_vector("esquerda", "direita", "cima", "baixo")
 		velocity = direction.normalized() * SPEED
 	else:
-		velocity = direction * DASHSPEED
+		velocity = direction.normalized() * DASHSPEED
 	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -20,34 +20,9 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Ataque"):
 		atacarInimigo()
 
-
-var listaInimigos : Array[InimigoBase] = []
-
-func _detectouInimigo(body: Node2D) -> void:
-	listaInimigos.append(body as InimigoBase)
-	pass # Replace with function body.
-
-func _inimigoSaiu(body: Node2D) -> void:
-	listaInimigos.erase(body as InimigoBase)
-
 func atacarInimigo():
-	if listaInimigos.is_empty(): 
-		dashing = true
-		return
-	
-	var alvo : InimigoBase = listaInimigos[0]
-	var dist = alvo.global_position.distance_squared_to(global_position)
-	var distTemp
-	for i in listaInimigos:
-		distTemp = i.global_position.distance_squared_to(global_position)
-		if distTemp < dist:
-			dist = distTemp
-			alvo = i
-	direction = alvo.global_position - global_position
-	direction = direction.normalized()
 	dashing = true
-	timer_dash.start(0.2)
-	alvo.tomouHit()
+	timer_dash.start(0.1)
 
 func _on_timer_dash_timeout() -> void:
 	dashing = false
